b18b6970a0c67b286847138324fe9e32d5d73a10
easymagic <nnamware@yahoo.com>
2017-08-12 02:18:13 -0700

    Update container.php
    syntactic sugar update....

--- base/core/container.php
+++ base/core/container.php
@@ -29,8 +29,71 @@
    }
 
 
+   function load_controller($ctrl,$alias=''){
+     
+     if (empty($alias)){
+       $name = $ctrl;
+     }else{
+       $name = $alias;
+     }
 
+     // echo $name;
+
+     $this->$name = $this->app->controller->$ctrl;
+
+   }
+
+
+   function load_model($mdl,$alias=''){
+     if (empty($alias)){
+       $name = $mdl;
+     }else{
+       $name = $alias;
+     }
+
+     $this->$name = $this->app->model->$mdl;
+
+   }
+
+
+   function load_use_case($usecase_parent,$usecase_child,$alias=''){
+     if (empty($alias)){
+        $name = $usecase_child;
+     }else{
+        $name = $alias;
+     }
+
+     $this->$name = $this->app->$usecase_parent->$usecase_child;
+
+   }
+
+
+   function load_lib($lib,$alias=''){
+     if (empty($alias)){
+       $name = $lib;
+     }else{
+       $name = $alias;
+     }
+
+     $this->$name = $this->app->lib->$lib;
+
+   }
+
+   function load_plugin($plg,$alias=''){
+
+     if (empty($alias)){
+       $name = $plg;
+     }else{
+       $name = $alias;
+     }
+
+     $this->$name = $this->app->plugin->$plg;
+
+   }
  
+
+
+
 
  }
 
